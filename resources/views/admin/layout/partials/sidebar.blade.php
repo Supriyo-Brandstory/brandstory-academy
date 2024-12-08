37469d0f15027a7a76db3b1c0120fc3e9098a8c6
Pradipta Bhuin <pradiptabhuin@Pradiptas-MacBook-Air.local>
2024-12-08 17:22:40 +0530

    dds

--- resources/views/admin/layout/partials/sidebar.blade.php
+++ resources/views/admin/layout/partials/sidebar.blade.php
@@ -9,55 +9,9 @@
             </a>
         </li>
         
-        <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#components-nav1" data-bs-toggle="collapse"
-                href="#">
-                <i class="bi bi-menu-button-wide"></i><span>Contacts</span><i
-                    class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="components-nav1" class="nav-content collapse {{(Route::is('clients.index') || Route::is('booking-types.index')) ? 'show' : ''}}" data-bs-parent="#sidebar-nav">
-                <li>
-                    <a class="{{ Route::is('clients.index') ? 'active' : ''}}" href="{{route('clients.index')}}">
-                        <i class="ri-group-fill"></i>
-                        <span>Clients</span>
-                    </a>
-                </li>
+        
 
-                
-            </ul>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#components-nav2" data-bs-toggle="collapse"
-                href="#">
-                <i class="bi bi-menu-button-wide"></i><span>System</span><i
-                    class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="components-nav2" class="nav-content collapse {{(Route::is('dispute-letters.index') || Route::is('booking-types.index')) ? 'show' : ''}}" data-bs-parent="#sidebar-nav">
-                <li>
-                    <a class="{{ Route::is('dispute-letters.index') ? 'active' : ''}}" href="{{route('dispute-letters.index')}}">
-                        <i class="ri-group-fill"></i>
-                        <span>Dispute Letter</span>
-                    </a>
-                </li>
-
-                
-            </ul>
-        </li>
-
-        {{-- <li class="nav-item ">
-            <a class="nav-link " href="#">
-                <i class="ri-group-fill"></i>
-                <span>Remunaration</span>
-            </a>
-        </li>
-
-        <li class="nav-item ">
-            <a class="nav-link " href="#">
-                <i class="ri-group-fill"></i>
-                <span>Event Mgmt</span>
-            </a>
-        </li> --}}
+       
         
 
     </ul>
