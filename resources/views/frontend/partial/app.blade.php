99d159f3646d30f8817c3da33f18c8f3f1cf4b8b
Supriyo-Brandstory <supriyo.brandstory.in>
2025-07-29 13:24:18 +0530

    ..

--- resources/views/frontend/partial/app.blade.php
+++ resources/views/frontend/partial/app.blade.php
@@ -5,6 +5,7 @@
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
   {!!$seo->script ?? '<title>#1 Digital Marketing Institute in Bangalore: BrandStory Academy</title>'!!}
   <link rel="preload" href="{{asset('frontend/assets/fonts/HankenGrotesk-Regular.woff2')}}" as="font" type="font/woff2"
     crossorigin>
@@ -18,6 +19,15 @@
 
 
   
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5SSJ8MF9');</script>
+<!-- End Google Tag Manager -->
+
+
 
 
   <!-- Meta Pixel Code -->
@@ -40,7 +50,10 @@
       src="https://www.facebook.com/tr?id=600881299379467&ev=PageView&noscript=1" /></noscript>
   <!-- End Meta Pixel Code -->
 
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SSJ8MF9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 </head>
 
