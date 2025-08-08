60dc026c398d554b012d48650fe5c58c4e805ca4
Supriyo-Brandstory <supriyo@brandstory.in>
2025-08-08 11:01:44 +0530

    for new

--- resources/views/frontend/partial/app.blade.php
+++ resources/views/frontend/partial/app.blade.php
@@ -19,13 +19,15 @@
   <link href="{{asset('frontend/assets/css/header.css')}}" rel="stylesheet">
 
 
-  
+<meta name="google-site-verification" content="y_Tb-RzvFLj9Pv4A4sc2MGZEC24q03sxiEdjIGbL04k" />
+
+
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5SSJ8MF9');</script>
+})(window,document,'script','dataLayer','GTM-5CF6TMZ5');</script>
 <!-- End Google Tag Manager -->
 
 
@@ -51,8 +53,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       src="https://www.facebook.com/tr?id=600881299379467&ev=PageView&noscript=1" /></noscript>
   <!-- End Meta Pixel Code -->
 
+
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SSJ8MF9"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CF6TMZ5"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
