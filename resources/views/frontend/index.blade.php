ea380f6bc9244c2164fe0bf64a5d530722f0b39e
tapasbrandstory <tapas@brandstory.in>
2025-09-03 13:02:25 +0530

    Home 2

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1063,6 +1063,12 @@
             </div>
         </div>
     </section>
+    <x-our-success-stories :videos="[
+ 'https://www.instagram.com/p/DNiDU0ExHZI',
+  'https://www.instagram.com/p/DNk72CQRPXZ',
+  'https://www.instagram.com/p/DNpgwppR5qy',
+  'https://www.instagram.com/p/DNk72CQRPXZ'
+]" />
 
 <div class="pt-100">
     <x-blog-component />
